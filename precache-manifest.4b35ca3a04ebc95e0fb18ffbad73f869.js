9f332ef615d75ff47dffb8022607d8749128cb1e
luvi01 <38668635+luvi01@users.noreply.github.com>
2019-10-23 21:30:34 -0300

    Updates

---
+++ precache-manifest.4b35ca3a04ebc95e0fb18ffbad73f869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b12ad798fd6219a09e68e3adb163fa5d",
+    "url": "/inlaw_1/index.html"
+  },
+  {
+    "revision": "b8c59133715be86dc29a",
+    "url": "/inlaw_1/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "464aa7a404d5eb62dcd5",
+    "url": "/inlaw_1/static/js/2.e8fd9e66.chunk.js"
+  },
+  {
+    "revision": "b8c59133715be86dc29a",
+    "url": "/inlaw_1/static/js/main.4f7b2c4e.chunk.js"
+  },
+  {
+    "revision": "341f22f365f48ce995ff",
+    "url": "/inlaw_1/static/js/runtime-main.e768796c.js"
+  }
+]);
